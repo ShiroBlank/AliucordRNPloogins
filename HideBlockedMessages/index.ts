8dfa00f99dd3bf81fdd5f6aaa7139651f4b9e5b6
shiro <teh1337taco@gmail.com>
2022-05-30 19:20:22 +0100

    inital setup or soemthing idk

---
+++ HideBlockedMessages/index.ts
@@ -0,0 +1,9 @@
+import { Plugin } from "aliucord/entities";
+import { getByProps } from "aliucord/metro";
+import { ApplicationCommandOptionType } from "aliucord/api";
+
+export default class HideBlockedMessages extends Plugin {
+    public async start() {
+        //piss
+    }
+}
